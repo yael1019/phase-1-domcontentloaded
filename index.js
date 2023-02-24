143ba3f793ed21970ddda6c880de8d227094531c
Yael Harosh <yaelharosh01@gmail.com>
2023-02-23 21:29:15 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const text = document.getElementById('text').textContent = 'This is really cool';
+    console.log(text);
+})
+
+console.log(document.getElementById('text'));
